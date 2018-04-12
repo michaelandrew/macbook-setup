b19e59c319bb3012a5a68ad9d3e15fb6c0bd4a77
Michael Andrew Chadwick <michaelandrew@users.noreply.github.com>
2018-04-12 10:39:30 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,5 +3,8 @@
 # Install Homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+# Install Brewfile
+brew bundle
+
 # Reset Dock
 defaults write com.apple.dock ResetLaunchPad -bool true; killall Dock
