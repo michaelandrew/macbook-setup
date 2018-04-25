6271096b8161835539b321839b50e0f168dd16c5
Michael Andrew Chadwick <michaelandrew@users.noreply.github.com>
2018-04-25 17:06:41 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,5 +9,8 @@ brew bundle
 # Install RVM
 \curl -sSL https://get.rvm.io | bash -s stable
 
+# Source RVM
+source ~/.rvm/scripts/rvm
+
 # Reset Dock
 defaults write com.apple.dock ResetLaunchPad -bool true; killall Dock
