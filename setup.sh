4a75c98e131dbb03387f5497e81b0085f2832f38
Michael Andrew Chadwick <michaelandrew@users.noreply.github.com>
2018-04-27 09:39:38 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -22,6 +22,5 @@ curl -O https://gitcdn.link/repo/mischah/Relaxed/master/themes/Relaxed.terminal
 # Install Oh My Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-
 # Reset Dock
 defaults write com.apple.dock ResetLaunchPad -bool true; killall Dock
