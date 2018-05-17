578b66940422f6b636576b7dbddb37a530569501
Michael Andrew Chadwick <michaelandrew@users.noreply.github.com>
2018-05-17 13:25:22 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-# Install Oh My Zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
 # Install Homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
